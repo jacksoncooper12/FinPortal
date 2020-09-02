e7f6951d2334a648fb28aeba10ff88b424995101
Jackson Cooper <jacksoncooper12@gmail.com>
2020-09-02 11:17:54 -0400

    Initial commit

---
+++ FinPortal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FinPortal.MvcApplication" Language="C#" %>
---
+++ FinPortal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinPortal.Startup))]
+namespace FinPortal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
